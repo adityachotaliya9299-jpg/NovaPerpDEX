6ae3539658afa9304b462dd8cc91fd4b9dd28ca1
Aditya <adityachotaliya9299@gmail.com>
2026-06-12 06:34:32 +0000

    feat(interfaces): add bad debt handler interface

--- src/interfaces/IBadDebtHandler.sol
+++ src/interfaces/IBadDebtHandler.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.26;
 
 /// @title IBadDebtHandler
-/// @author Aditya Chotaliya [adityachotaliya.vercel.app]
+/// @author Aditya Chotaliya [adityachotaliya.xyz]
 /// @notice Interface the CollateralVault uses to record socialized bad debt when the
 ///         insurance fund cannot fully cover a liquidation shortfall.
 interface IBadDebtHandler {
