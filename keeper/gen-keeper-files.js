0636baed6de6331603453ccab554e4aa14467662
Aditya <adityachotaliya9299@gmail.com>
2026-06-20 11:47:07 +0000

    feat(keeper):new deployment json

--- keeper/gen-keeper-files.js
+++ keeper/gen-keeper-files.js
@@ -1,5 +1,9 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ROOT = path.resolve(__dirname, "..");
 const abisContent = fs.readFileSync(path.join(ROOT, "frontend/lib/abis.ts"), "utf8");
